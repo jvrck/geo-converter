fc0d793452b9ef1d89711ec6ae07c3bb4a0b72c1
Jim Vrckovski <jim.vrckovski@fatzebra.com.au>
2020-06-25 15:10:00 +1000

    updated workflow

--- test.sh
+++ test.sh
@@ -4,9 +4,13 @@ echo "++++ test.sh ++++"
 
 cd /data
 
+ls -la
+
 # Process files in /data
+# Unzip shape files
 for f in *.zip; do
     unzip -n $f
 done
 
+# Execute conversion script
 sh /app/convert.sh
